655e83db9d65ff07189e867425775a205541f3e5
retter-berkeley <retter@berkeley.edu>
2025-06-27 14:36:23 -0400

    add drive

--- .ipynb_checkpoints/DriveXP_test-checkpoint.py
+++ .ipynb_checkpoints/DriveXP_test-checkpoint.py
@@ -5,9 +5,9 @@ import sys
 from PI_Drive import PythonInterface as pi
 
 sys.path.insert(0, '/home/amninder/Desktop/Folder_2')
-time.sleep(3)
+
 action=np.array([0.,0.,0.,])
 while action[0]<1:
     pi.ApplyValues(action)
     action=action+np.array([0.05,0.05,0.05])
-    
+    time.sleep(3)
