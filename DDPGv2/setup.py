2229515f7d1fb9813b0713f72a29f5c1dca7fcb2
retter-berkeley <retter@berkeley.edu>
2025-10-25 16:33:09 -0400

    update for all in XP Python

---
+++ DDPGv2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='DDPG',
+    packages=find_packages(),
+    version='0.1.0',
+    description='DDPG implementation library',
+    author='Me',
+)
